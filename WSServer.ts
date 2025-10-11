eb20d90e2156aa37b6b9af679cac10c4d4479e0c
Minimouse <473147334@qq.com>
2025-10-11 15:52:29 +0800

    优化代码结构

--- WSServer.ts
+++ WSServer.ts
@@ -33,168 +33,176 @@ export function rejectAsNotAuthed(serverName:string,client:ws){
         msg:"服务器"+serverName+"尚未验证！"
     })
 }
+type handlerFunction=(data:any,serverName:string,requestUID:string,rawData:any,client:ws)=>void
+const handlers:Map<string,{
+    authRequired:boolean,
+    handler:handlerFunction
+}>=new Map()
 //发心跳包防止长连接断开
 //心跳包不需要鉴权
 setInterval(()=>wsServer.clients.forEach(client=>sendHeartbeat(client)),9000)
 wsServer.on("connection",client=>{
-    client.on("message",rawData=>{
-        try{
-            const parsedData=JSON.parse(rawData.toString());
-            const {type,data,serverName,requestUID}=parsedData
-            //先检查是否有serverName，如果没有则直接拒绝
-            if(serverName==undefined){
-                sendClientError(client,{
-                    type:"error",
-                    error:"pattern_not_provided",
-                    serverName:"not_provided",
-                    msg:"发送的数据中缺少serverName字段",
-                    attachments:{pattern:"serverName"}
-                })
-                return;
-            }
-            if(config.get("servers")[serverName]==undefined){
-                sendClientError(client,{
-                    type:"error",
-                    error:"server_not_exist",
-                    msg:serverName+"不存在！",
-                    serverName
-                })
-                return;
-            }
-            switch(type){
-                //鉴权用API
-                case "auth":
-                    if(authClient(client,serverName,parsedData.token)){
-                        sendAuthedBack(client,{
-                            type:"authed",
-                            requestUID
-                        })
-                    }
-                    else{
-                        rejectAsNotAuthed(serverName,client)
-                    }
-                    break;
-                //需要鉴权
-                case "execute":
-                    if(checkClientAuthed(serverName,client)){
-                        serverSessions.get(serverName).session.execute([data]);
-                        //如果客户端尝试执行了命令，那么激活相应会话
-                        serverSessions.get(serverName).activeManager.activate()
-                    }
-                    else rejectAsNotAuthed(serverName,client)
-                    break;
-                //需要鉴权
-                case "fetch_all_server_logs":
-                    if(checkClientAuthed(serverName,client))sendFetchAllServerLogsResultPack(client,{
-                        type:"fetch_all_server_logs_result",
-                        data:serverSessions.get(serverName).session.logs,
-                        requestUID
-                    })
-                    else rejectAsNotAuthed(serverName,client)
-                    break;
-                //公开
-                case "fetch_process_status":
-                    sendFetchProcessStatusResultPack(client,{
-                        type:"fetch_process_status_result",
-                        data:serverSessions.get(serverName).lastStatus,
-                        requestUID
-                    })
-                    break;
-                //公开
-                case "fetch_hardware_status":
-                    new Promise<HardwareStatusResult>((resolve,reject)=>{
-                        switch(config.get("servers")[serverName].performance?.type){
-                            case "local":{
-                                getThisMachineHardwareStatus().then(result=>resolve(result))
-                                break;
-                            }
-                            case undefined:
-                            case "native":{
-                                serverSessions.get(serverName).session.getHardwareStatus()
-                                    .then(result=>resolve(result))
-                                    .catch(reason=>{
-                                        if(reason instanceof DLSAPIError){
-                                            if(reason.code==="NETWORKTIMEDOUT")sendClientError(client,{
-                                                type:"error",
-                                                msg:"请求目标服务器的硬件状态超时。",
-                                                serverName,
-                                                error:"DLSAPIError"
-                                            })
-                                            else sendClientError(client,{
-                                                type:"error",
-                                                msg:"请求目标服务器的硬件状态时DLSProtocol报错"+reason,
-                                                serverName,
-                                                error:"DLSAPIError"
-                                            })
-                                        }
-                                        else{
-                                            sendClientError(client,{
-                                                type:"error",
-                                                msg:"请求硬件状态时发生无法处理的错误："+reason,
-                                                serverName,
-                                                error:"internal_server_error",
-                                                attachments:{
-                                                    error:JSON.stringify(reason)
-                                                }
-                                            })
-                                        }
-                                    })
-                                break;
-                            }
-                            case "detector":{
-                                //由于这里是直接读取原始请求，所以需要对其重新编码
-                                //考虑服务器未开启、端口填错等情况
-                                SimpleHTTPReq.GET(config.get("servers")[serverName].performance?.address+"/server_status")
-                                    .then(result=>resolve(JSON.parse(result.responseData)))
-                                    .catch(e=>{
-                                        reject({
-                                            code:503,
-                                            msg:"连接DLSPerformanceDetector失败！"
-                                        })
-                                    })
-                                    
-                                break;
-                            }
-                            default:throw new Error("不支持的硬件信息来源："+config.get("servers")[serverName].performance?.type)
-                        }                        
-                    }).then(result=>{
-                        sendFetchHardwareStatusResultPack(client,{
-                            type:"fetch_hardware_status_result",
-                            data:result,
-                            requestUID
-                        })
-                    }).catch(reason=>{
-                        sendRequestFailed(client,{
-                            type:"request_falied",
-                            requestType:type,
-                            serverName,
-                            requestUID,
-                            attachments:reason
-                        })
-                    })
-
-                    break;
-                default:
-                    //错误不再从这里抛出，防止被客户端搞死
-                    //直接返回给客户端，让客户端自行处理
-                    sendClientError(client,{
-                        type:"error",
-                        error:"type_not_supported",
-                        serverName,
-                        msg:"不支持的行为："+type,
-                        attachments:{
-                            type
-                        }
-                    });
-            }
+    client.on("message",rawData=>{try{
+        const parsedData=JSON.parse(rawData.toString());
+        const {type,data,serverName,requestUID}=parsedData
+        //先检查是否有serverName，如果没有则直接拒绝
+        if(serverName==undefined){
+            sendClientError(client,{
+                type:"error",
+                error:"pattern_not_provided",
+                serverName:"not_provided",
+                msg:"发送的数据中缺少serverName字段",
+                attachments:{pattern:"serverName"}
+            })
+            return;
         }
-        catch(e){
-            Logger.error("无法解析客户端 "+"[暂不支持显示客户端id]"+" 发来的数据：\n"+e)
-            Logger.error("数据内容："+rawData.toString())
+        if(config.get("servers")[serverName]==undefined){
+            sendClientError(client,{
+                type:"error",
+                error:"server_not_exist",
+                msg:serverName+"不存在！",
+                serverName
+            })
+            return;
         }
-    })
+        //寻找数据包type
+        const handler=handlers.get(type)
+        if(handler===undefined){
+            //错误不再从这里抛出，防止被客户端搞死
+            //直接返回给客户端，让客户端自行处理
+            sendClientError(client,{
+                type:"error",
+                error:"type_not_supported",
+                serverName,
+                msg:"不支持的行为："+type,
+                attachments:{
+                    type
+                }
+            });
+            return;
+        }
+        if(handler.authRequired&&!checkClientAuthed(serverName,client)) rejectAsNotAuthed(serverName,client);
+        else handler.handler(data,serverName,requestUID,parsedData,client)
+    }
+    catch(e){
+        Logger.error("无法解析客户端 "+"[暂不支持显示客户端id]"+" 发来的数据：\n"+e)
+        Logger.error("数据内容："+rawData.toString())
+    }})
     client.on('close',ws=>{
         //客户端断开连接
     })
 })
+
+function setTypeHandler(type:string,handler:handlerFunction,authRequired:boolean=true){
+    handlers.set(type,{
+        handler,
+        authRequired
+    })
+}
+
+setTypeHandler("auth",(data,serverName,requestUID,rawData,client)=>{
+    if(authClient(client,serverName,rawData.token)){
+        sendAuthedBack(client,{
+            type:"authed",
+            requestUID
+        })
+    }
+    else rejectAsNotAuthed(serverName,client)
+},false)
+
+setTypeHandler("execute",(data,serverName,requestUID,rawData,client)=>{
+    serverSessions.get(serverName).session.execute([data]);
+    //如果客户端尝试执行了命令，那么激活相应会话
+    serverSessions.get(serverName).activeManager.activate()
+})
+
+setTypeHandler("fetch_all_server_logs",(data,serverName,requestUID,rawData,client)=>{
+    sendFetchAllServerLogsResultPack(client,{
+        type:"fetch_all_server_logs_result",
+        data:serverSessions.get(serverName).session.logs,
+        requestUID
+    })
+})
+
+setTypeHandler("fetch_process_status",(data,serverName,requestUID,rawData,client)=>{
+    sendFetchProcessStatusResultPack(client,{
+        type:"fetch_process_status_result",
+        data:serverSessions.get(serverName).lastStatus,
+        requestUID
+    })
+},false)
+
+setTypeHandler("fetch_hardware_status",(data,serverName,requestUID,rawData,client)=>{
+    new Promise<HardwareStatusResult>((resolve,reject)=>{
+        switch(config.get("servers")[serverName].performance?.type){
+            case "local":{
+                getThisMachineHardwareStatus().then(result=>resolve(result))
+                break;
+            }
+            case undefined:
+            case "native":{
+                serverSessions.get(serverName).session.getHardwareStatus()
+                    .then(result=>resolve(result))
+                    .catch(reason=>{
+                        if(reason instanceof DLSAPIError){
+                            if(reason.code==="NETWORKTIMEDOUT")sendClientError(client,{
+                                type:"error",
+                                msg:"请求目标服务器的硬件状态超时。",
+                                serverName,
+                                error:"DLSAPIError"
+                            })
+                            else sendClientError(client,{
+                                type:"error",
+                                msg:"请求目标服务器的硬件状态时DLSProtocol报错"+reason,
+                                serverName,
+                                error:"DLSAPIError"
+                            })
+                        }
+                        else{
+                            sendClientError(client,{
+                                type:"error",
+                                msg:"请求硬件状态时发生无法处理的错误："+reason,
+                                serverName,
+                                error:"internal_server_error",
+                                attachments:{
+                                    error:JSON.stringify(reason)
+                                }
+                            })
+                        }
+                    })
+                break;
+            }
+            case "detector":{
+                //由于这里是直接读取原始请求，所以需要对其重新编码
+                //考虑服务器未开启、端口填错等情况
+                SimpleHTTPReq.GET(config.get("servers")[serverName].performance?.address+"/server_status")
+                    .then(result=>resolve(JSON.parse(result.responseData)))
+                    .catch(e=>{
+                        reject({
+                            code:503,
+                            msg:"连接DLSPerformanceDetector失败！"
+                        })
+                    })
+                    
+                break;
+            }
+            default:throw new Error("不支持的硬件信息来源："+config.get("servers")[serverName].performance?.type)
+        }                        
+    }).then(result=>{
+        sendFetchHardwareStatusResultPack(client,{
+            type:"fetch_hardware_status_result",
+            data:result,
+            requestUID
+        })
+    }).catch(reason=>{
+        sendRequestFailed(client,{
+            type:"request_falied",
+            requestType:"fetch_hardware_status",
+            serverName,
+            requestUID,
+            attachments:reason
+        })
+    })
+},false)
+
 
